7476fbb359df8c251bf48431d2bbff10a8cb658d
Murat AKMAMEDAU <muratx10@gmail.com>
2020-03-02 22:01:45 +0300

    fix: audio preload

--- src/components/Bird/Audio/Audio.js
+++ src/components/Bird/Audio/Audio.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Audio = ({ src }) => (
   <div className="audioplayer ml-2">
     {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-    <audio src={src} controls width="100%" />
+    <audio src={src} controls preload="auto" />
   </div>
 );
 
